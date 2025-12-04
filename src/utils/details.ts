a4bb907f980e767e142acffae8df1b733b7b45bf
Josue Marchena <josuemarchena0730@gmail.com>
2025-12-04 17:54:14 -0500

    Translated Info to EN

--- src/utils/details.ts
+++ src/utils/details.ts
@@ -5,26 +5,26 @@ type Skill = {
 };
 
 export const skills: Skill[] = [
-  { name: "HTML", level: "Avanzado", icon: "/skills/html.svg" },
-  { name: "CSS", level: "Avanzado", icon: "/skills/css.svg" },
-  { name: "JavaScript", level: "Avanzado", icon: "/skills/javascript.svg" },
-  { name: "TypeScript", level: "Intermedio", icon: "/skills/typescript.svg" },
-  { name: "Tailwind CSS", level: "Avanzado", icon: "/skills/tailwindcss.svg" },
-  { name: "Node.js", level: "Avanzado", icon: "/skills/nodejs.svg" },
-  { name: "Astro", level: "Avanzado", icon: "/skills/astro.svg" },
-  { name: "Python", level: "Intermedio", icon: "/skills/python.svg" },
-{name: "React", level: "Intermedio", icon: "/skills/react.svg" },
-{name: "Next JS", level: "Básico", icon: "/skills/nextjs.svg" },
-{name: "MongoDB", level: "Intermedio", icon: "/skills/mongodb.svg" },
-  { name: "Express.js", level: "Intermedio", icon: "/skills/expressjs.svg" },
-  { name: "SQL", level: "Avanzado", icon: "/skills/sql.svg" },
-  { name: "SQLite", level: "Intermedio", icon: "/skills/sqlite.svg" },
-  { name: "Figma", level: "Avanzado", icon: "/skills/figma.svg" },
-  { name: "UX/UI", level: "Avanzado", icon: "/skills/ui.svg" },
-  { name: "Gsap", level: "Intermedio", icon: "/skills/gsap.svg" },
-  { name: "Git", level: "Avanzado", icon: "/skills/git.svg" },
-  { name: "Github", level: "Avanzado", icon: "/skills/github.svg" },
-  { name: "Terminal", level: "Avanzado", icon: "/skills/terminal.svg" }
+  { name: "HTML", level: "Advanced", icon: "/skills/html.svg" },
+  { name: "CSS", level: "Advanced", icon: "/skills/css.svg" },
+  { name: "JavaScript", level: "Advanced", icon: "/skills/javascript.svg" },
+  { name: "TypeScript", level: "Intermediate", icon: "/skills/typescript.svg" },
+  { name: "Tailwind CSS", level: "Advanced", icon: "/skills/tailwindcss.svg" },
+  { name: "Node.js", level: "Advanced", icon: "/skills/nodejs.svg" },
+  { name: "Astro", level: "Advanced", icon: "/skills/astro.svg" },
+  { name: "Python", level: "Intermediate", icon: "/skills/python.svg" },
+  { name: "React", level: "Intermediate", icon: "/skills/react.svg" },
+  { name: "Next JS", level: "Basic", icon: "/skills/nextjs.svg" },
+  { name: "MongoDB", level: "Intermediate", icon: "/skills/mongodb.svg" },
+  { name: "Express.js", level: "Intermediate", icon: "/skills/expressjs.svg" },
+  { name: "SQL", level: "Advanced", icon: "/skills/sql.svg" },
+  { name: "SQLite", level: "Intermediate", icon: "/skills/sqlite.svg" },
+  { name: "Figma", level: "Advanced", icon: "/skills/figma.svg" },
+  { name: "UX/UI", level: "Advanced", icon: "/skills/ui.svg" },
+  { name: "Gsap", level: "Intermediate", icon: "/skills/gsap.svg" },
+  { name: "Git", level: "Advanced", icon: "/skills/git.svg" },
+  { name: "Github", level: "Advanced", icon: "/skills/github.svg" },
+  { name: "Terminal", level: "Advanced", icon: "/skills/terminal.svg" }
 ];
 
 export const skillsPerSlide: number = 4;
